352ffb226bf44ad80a1740ebd884ea2fbe0ccb9f
sdor <sergey.doronin@stonybrook.edu>
2019-11-16 08:06:54 -0500

    wip

--- src/main/scala/com/biosystechnologies/peptides/AminoAcidSeq.scala
+++ src/main/scala/com/biosystechnologies/peptides/AminoAcidSeq.scala
@@ -1,6 +1,8 @@
 package com.biosystechnologies.peptides
 
-case class AminoAcidSeq(seq: Array[AminoAcid],modifications: List[AminoAcidSeqModification]=List())
+case class AminoAcidSeq(seq: Array[AminoAcid], modifications: List[AminoAcidSeqModification]=List()) {
+    
+}
 
 object AminoAcidSeq {
 
