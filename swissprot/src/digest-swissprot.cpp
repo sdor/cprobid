7b3c63e8c91cc1f96fcdc753208e1885dbcaa612
sdor <sergey.doronin@stonybrook.edu>
2019-12-11 05:39:35 -0500

    шип

--- swissprot/src/digest-swissprot.cpp
+++ swissprot/src/digest-swissprot.cpp
@@ -6,18 +6,40 @@
 #include <algorithm>
 #include <swissprot.h>
 #include <AminoAcidSeq.h>
+#include <bsoncxx/builder/stream/array.hpp>
 #include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/types.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+
 
 using namespace std;
 using namespace swissprot;
-using bsoncxx::builder::stream::document;
+using namespace bsoncxx;
+
+typedef struct {
+  mongocxx::database* db;
+} metadata;
+
+typedef struct {
+  metadata* metadata;
+  void* data;
+} swissprot_reader_data;
 
 void reader(void *data) {
+  using builder::stream::document;
+  using builder::stream::array;
+  using builder::stream::open_document;
+  using builder::stream::close_document;
+  using bsoncxx::builder::stream::finalize;
+  mongocxx::collection peptides = (*((swissprot_reader_data*) data)->metadata->db)["peptides"];
   auto doc = xmlReadMemory((const char*)data,strlen((const char *)data),NULL,NULL,XML_PARSE_NOBLANKS);
   auto ctx = xmlXPathNewContext(doc);
   xmlXPathRegisterNs(ctx,(const xmlChar*)"sw",(const xmlChar*)"http://uniprot.org/uniprot");
-  string name = parseName(ctx);
-  string protein = parseProteinRecommendedName(ctx);
+  std::string name = parseName(ctx);
+  std::string protein = parseProteinRecommendedName(ctx);
   organism organism = parseOrganism(ctx);
   sequence sequence = parseSequence(ctx);
   xmlXPathFreeContext(ctx);
@@ -28,7 +50,22 @@ void reader(void *data) {
       for (auto it = digest.begin(); it != digest.end(); ++it) {
         auto n_missed = it->first;
         for(auto &seq: it->second) {
-          
+ 
+          peptides.find_one_and_update(document{}
+            << "name" << name
+            << "organism" << organism.scientific_name
+            << "n_missed" << static_cast<int>(n_missed)
+            << "peptide" << seq.toString()
+            << finalize,
+            document{} 
+              << "$set" << open_document
+                  << "name" << name
+                  << "organism" << organism.scientific_name
+                  << "n_missed" << static_cast<int>(n_missed)
+                  << "peptide" << seq.toString()
+                  << close_document  
+              << finalize
+          );
         }
       }
       
